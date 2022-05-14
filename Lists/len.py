2ec704b354bd83c941e47e5bc2e1bd131a2b1f7a
djumanov <diyorbekjumanov1120@gmail.com>
2022-05-14 12:25:44 +0500

    len

---
+++ Lists/len.py
@@ -0,0 +1,31 @@
+# The len() function returns the number of items of an object.
+
+# number of characters in a string
+S = 'Python'
+x = len(S)
+print(x)
+# Prints 6
+
+# number of items in a list
+L = ['red', 'green', 'blue']
+x = len(L)
+print(x)
+# Prints 3
+
+# number of items in a tuple
+T = ('red', 'green', 'blue')
+x = len(T)
+print(x)
+# Prints 3
+
+# number of key:value pairs in a dictionary
+D = {'name': 'Bob', 'age': 25}
+x = len(D)
+print(x)
+# Prints 2
+
+# number of items in a set
+S = {'red', 'green', 'blue'}
+x = len(S)
+print(x)
+# Prints 3
