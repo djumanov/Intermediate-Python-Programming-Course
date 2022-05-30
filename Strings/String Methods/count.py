d29f5b4d13c4906416831581687ba92c896767cc
djumanov <diyorbekjumanov1120@gmail.com>
2022-05-30 14:46:09 +0500

    count method

---
+++ Strings/String Methods/count.py
@@ -0,0 +1,18 @@
+# Count occurrences of 'Big' in the string
+S = 'Big, Bigger, Biggest'
+x = S.count('Big')
+print(x)
+# Prints 3
+
+# Count occurrences of 'Big' from 5th character
+S = 'Big, Bigger, Biggest'
+x = S.count('Big',5)
+print(x)
+# Prints 2
+
+# Count occurrences of 'Big' between 5th to 13th character
+S = 'Big, Bigger, Biggest'
+x = S.count('Big',5,13)
+print(x)
+# Prints 1
+
