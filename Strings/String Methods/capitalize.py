6b8d57aad5d634c3708edfe4120687f0b726d206
djumanov <diyorbekjumanov1120@gmail.com>
2022-05-30 14:28:21 +0500

    capitalize method

---
+++ Strings/String Methods/capitalize.py
@@ -0,0 +1,10 @@
+# Capitalize the string
+S = 'bob is a CEO at ABC.'
+x = S.capitalize()
+print(x)
+# Prints Bob is a ceo at abc.
+
+S = '42 is my FAVOURITE number.'
+x = S.capitalize()
+print(x)
+# Prints 42 is my favourite number.
